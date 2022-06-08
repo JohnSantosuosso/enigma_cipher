e364b3674f297c3ed465b0f6a9fc5a6aad7230a7
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 21:39:02 -0400

    update tests in enigma spec for key generation

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,3 +10,17 @@ RSpec.describe Enigma do
       expect(@enigma).to be_instance_of(Enigma)
     end
   end
+
+  it 'returns the same key when provided' do
+    @enigma.key_check("02715")
+    expect(@enigma.key).to eql("02715")
+    expect(@enigma.key).to be_a(String)
+  end
+
+  it 'generates a new key when key is not provided' do
+    @enigma.key_check(nil)
+    expect(@enigma.key).not_to eql(nil)
+    expect(@enigma.key).to be_a(String)
+  end
+
+end
