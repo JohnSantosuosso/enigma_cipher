069e08097036ff4c777e173f0489bd62a5bcc04b
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 23:07:57 -0400

    update enigma with new modules

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,26 @@
 require_relative '../modules/creatable'
+require_relative '../modules/shiftable'
+require_relative '../modules/alphabet'
+require_relative '../lib/encryption'
 
 class Enigma
   include Creatable
 
-  attr_accessor :key, :date
+  attr_accessor :key, :date, :message, :encryption, :decryption
 
   def initialize
     @key = key
     @date = date
+    @message = message
+    @encryption = Encryption.new
+    @decryption = Decryption.new
   end
 
   def encrypt(message, key=nil, date=nil)
     key_check(key)
     date_check(date)
+    message_check(message)
+    @encryption.run_encryption(message, @key, @date)
   end
 
   def key_check(key)
