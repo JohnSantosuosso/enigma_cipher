eaf574d25cee421fbdf654cb4bcff497a8b3899f
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-08 01:24:08 -0400

    update enigma to include generate shifts from module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,34 +1,25 @@
 require_relative '../modules/creatable'
-require_relative '../modules/shiftable'
-require_relative '../modules/alphabet'
-require_relative '../lib/encryption'
+require_relative 'encryption'
 
 class Enigma
   include Creatable
 
-  attr_accessor :key, :date, :message, :encryption, :decryption
+  attr_accessor :key, :date, :shift, :message, :encryption
 
   def initialize
     @key = key
     @date = date
     @message = message
+    @shift = shift
     @encryption = Encryption.new
-    @decryption = Decryption.new
   end
 
   def encrypt(message, key=nil, date=nil)
-    key_check(key)
-    date_check(date)
-    message_check(message)
-    @encryption.run_encryption(message, @key, @date)
-  end
-
-  def key_check(key)
-    key.nil? == true ? generate_new_key : @key = key
-  end
-
-  def date_check(date)
-    date.nil? == true ? generate_new_date : @date = date
+    generate_key(key)
+    generate_date(date)
+    generate_shifts
+    generate_message
+    @encryption.run_encryption(@message, @shifts)
   end
 
 end
