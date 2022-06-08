aa11e79cb30184e5c6c111724079b3270ee6548b
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 21:58:26 -0400

    update creatable module for date generation

--- modules/creatable.rb
+++ modules/creatable.rb
@@ -4,4 +4,9 @@ module Creatable
     @key = (0..9).to_a.sample(5).join
   end
 
+  def generate_new_date
+    @date = Time.now.strftime('%m/%d/%Y').sub('20','')
+    @date.gsub!('/', '').to_s[-4..-1]
+  end
+
 end
