5ebf5c64ce49a28d1eb7fb7d27eb24385e216136
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 21:58:39 -0400

    update enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,19 +3,24 @@ require_relative '../modules/creatable'
 class Enigma
   include Creatable
 
-  attr_accessor :date, :key
+  attr_accessor :key, :date
 
   def initialize
-    @date = date
     @key = key
+    @date = date
   end
 
   def encrypt(message, key=nil, date=nil)
     key_check(key)
+    date_check(date)
   end
 
   def key_check(key)
     key.nil? == true ? generate_new_key : @key = key
+  end
+
+  def date_check(date)
+    date.nil? == true ? generate_new_date : @date = date
   end
 
 end
