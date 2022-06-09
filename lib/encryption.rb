a124bd2cc898597e5dbe88a9da2f3da1cfe7cdcd
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-09 15:26:19 -0400

    add additional tests to encryption spec and encryption class

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -4,18 +4,19 @@ require_relative 'enigma'
 class Encryption
   include Creatable
 
-  attr_accessor :message, :shifts #dry with variable names
+  attr_accessor :message, :shifts, :formatted_message #dry with variable names
 
   def initialize(message,shifts) #how to pull these from enigma???
     @message = message
     @shifts = shifts
+    @formatted_message = formatted_message
   end
 
   def add_shifts
-    format_message = @message.each_slice(4).to_a
+    @formatted_message = @message.each_slice(4).to_a
     #left off here--need tests
   end
 
-
+  def 
 
 end
