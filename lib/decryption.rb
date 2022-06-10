7691e2ef64325b505221536dc6bdbb3133260d7e
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-09 23:27:44 -0400

    add all methods inside run decryption method

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -11,6 +11,14 @@ class Decryption
     @final_message_form = final_message_form
   end
 
+  def run_decryption_on_message
+    generate_formatted_message
+    map_decrypt_shifts_to_characters
+    format_negative_numbers
+    generate_alphanumeric_replacements
+    generate_final_message_form
+  end
+
   def map_decrypt_shifts_to_characters
     @formatted_message = @formatted_message.map! {|message| message.zip(@shifts)}.flatten(1).map! {|values| values[0]-values[1]}
   end
