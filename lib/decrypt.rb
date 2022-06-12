f013733dcff88c4c59719de902a129339f2ca97f
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-12 11:44:19 -0400

    add hardcoded key date time to cli

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,15 +1,19 @@
 require_relative 'enigma'
 require_relative 'decryption'
+
 message = File.open(ARGV[0], 'r')#opens file class
 
 incoming_text = message.read #gives access to the message contents
 
 message.close #memory management, get what you need, close it
 
+output_date = output_text[-7..-1][0..-3]
+output_key = output_text[-30..-1][0..-20][-5..-1]
+
 enigma = Enigma.new
 enigma.message = incoming_text.strip#removes trailing \n
-enigma.key = ARGV[2]
-enigma.date = ARGV[3]
+enigma.key = output_date
+enigma.date = output_key
 unaltered_text = enigma.decrypt(enigma.message, enigma.key, enigma.date)
 
 writer = File.open(ARGV[1], 'w')
@@ -17,3 +21,7 @@ writer = File.open(ARGV[1], 'w')
 writer.write(unaltered_text)#could be instance of class file, .write instance of class file
 
 writer.close #memory management
+
+time = Time.now.strftime("%d/%m/%Y %H:%M")
+
+puts "Created #{ARGV[1]} at #{time} with the key #{output_key} and the date #{output_date}."
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,5 +1,6 @@
 require_relative 'enigma'
 require_relative 'encryption'
+
 message = File.open(ARGV[0], 'r')#opens file class
 
 incoming_text = message.read #gives access to the message contents
@@ -17,3 +18,15 @@ writer = File.open(ARGV[1], 'w')
 writer.write(unaltered_text)#could be instance of class file, .write instance of class file
 
 writer.close #memory management
+
+reader = File.open(ARGV[1], 'r')
+
+output_text = reader.read
+
+reader.close
+
+output_date = output_text[-7..-1][0..-3]
+output_key = output_text[-30..-1][0..-20][-5..-1]
+time = Time.now.strftime("%d/%m/%Y %H:%M")
+
+puts "Created #{ARGV[1]} at #{time} with the key #{output_key} and the date #{output_date}."
